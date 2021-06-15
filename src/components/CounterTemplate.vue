19f617f0ed154f28534f8fb6ebdedad1cc5572bc
Afshar07 <mr.afshar0@gmail.com>
2021-06-16 04:19:12 +0430

    use toFixed method for arranging decimals

--- src/components/CounterTemplate.vue
+++ src/components/CounterTemplate.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="counter-container">
     <h2>Step {{ this.index + 1 }}</h2>
-    <h2>{{ this.steps }}</h2>
+    <h2>{{ this.defaultValue }}</h2>
     <button @click.prevent="runCounter(this.stepValue)">RUN</button>
   </div>
 </template>
@@ -12,11 +12,14 @@ export default {
   data() {
     return {
       steps: parseFloat(this.stepValue),
+      defaultValue: 0,
     };
   },
+
   methods: {
     runCounter(value) {
-      this.steps = this.steps + parseFloat(value);
+      let newestValue = parseFloat(this.defaultValue) + parseFloat(value);
+      this.defaultValue = newestValue.toFixed(1);
     },
   },
 };
