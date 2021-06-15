7fb4bcc5461877e3bceca4660c1665601d41994d
Afshar07 <mr.afshar0@gmail.com>
2021-06-16 04:01:42 +0430

    Add the logic for CounterTemplate step button

---
+++ src/components/CounterTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="counter-container">
+    <h2>Step {{ this.index + 1 }}</h2>
+    <h2>{{ this.steps }}</h2>
+    <button @click.prevent="runCounter(this.stepValue)">RUN</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["stepValue", "index"],
+  data() {
+    return {
+      steps: parseFloat(this.stepValue),
+    };
+  },
+  methods: {
+    runCounter(value) {
+      this.steps = this.steps + parseFloat(value);
+    },
+  },
+};
+</script>
