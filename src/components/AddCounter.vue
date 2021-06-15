e66b3c445e1c8da3507d5560287dfdc05dc74a6a
Afshar07 <mr.afshar0@gmail.com>
2021-06-15 17:07:51 +0430

    Handle the input one decimal only with RegEx

---
+++ src/components/AddCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <input type="number" v-model="inputNumber" @input="isValid" />
+  </div>
+</template>
+
+<style scoped></style>
+<script>
+export default {
+  data() {
+    return {
+      inputNumber: null,
+    };
+  },
+  methods: {
+    isValid() {
+      this.inputNumber = this.inputNumber.match(/^-?\d+\.?\d{0,1}/);
+    },
+  },
+};
+</script>
