040631186d07826ea4ab5b434bac9a4e81143d1f
Afshar07 <mr.afshar0@gmail.com>
2021-06-16 21:30:02 +0430

    Add style to component

--- src/components/CounterTemplate.vue
+++ src/components/CounterTemplate.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="counter-container">
-    <h2>Step {{ this.index + 1 }}</h2>
+    <div class="counter-title">
+      <h2 class="title">Step {{ this.index + 1 }}</h2>
+    </div>
+      <p class="step-value">Value: {{ this.stepValue }}</p>
     <h2>{{ this.defaultValue }}</h2>
-    <button @click.prevent="runCounter(this.stepValue)">RUN</button>
+    <button @click.prevent="runCounter(this.stepValue)" class="add-button">
+      RUN
+    </button>
   </div>
 </template>
 
@@ -33,3 +38,45 @@ export default {
   },
 };
 </script>
+<style>
+.counter-container {
+  margin: 2rem 3rem 1rem 3rem;
+  border-radius: 5px;
+  border: 1px solid #08a878;
+  border-top: 3px solid #08a878;
+  display: flex;
+  flex-direction: column;
+  min-width: 20vw;
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+}
+.counter-title {
+  display: flex;
+  border-bottom: 1px solid #487468;
+}
+.title {
+  padding: 0.6rem 0.5rem 0.5rem 0.3rem;
+  margin: 0;
+  font-size: 1rem;
+  font-weight: normal;
+  color: #005a3f;
+}
+.step-value {
+  padding: 0.6rem 0.5rem 0.5rem 0.3rem;
+  margin: 0;
+  font-size: 0.85rem;
+}
+.add-button {
+  width: 50%;
+  height: 1.7rem;
+  margin-bottom: 1rem;
+  align-self: center;
+  background-color: transparent;
+  border: 1px solid #08a878;
+  border-radius: 20px;
+  cursor: pointer;
+}
+.add-button:hover {
+  background-color: #08a878;
+  color: #fff;
+}
+</style>
